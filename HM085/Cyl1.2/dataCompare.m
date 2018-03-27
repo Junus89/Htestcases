3f448f2534cf96b5fda079a72afcdaded5b33529
Yunusi Fuerkaiti <yunusi@stro024.vub.ac.be>
2018-03-27 07:07:25 +0200

    debugged for linux system

--- HM085/Cyl1.2/dataCompare.m
+++ HM085/Cyl1.2/dataCompare.m
@@ -12,31 +12,31 @@ set(0,'DefaultaxesFontSize',15)
 %
 
 
-% NFFT=8192;
-% 
-% MaX=0.0; MaY=0.0;MaZ=0.0;
-% OmegaR = 2.0*pi*27.0;
-% TR = 2.0*pi/OmegaR;
-% fR = 1.0/TR;
+NFFT=16;
+C0=347.18;RMatip=1.045;Ma = 0.85;
+MaX=0.0; MaY=0.0;MaZ=0.0;
+OmegaR = C0*0.85/RMatip;
+TR = 2.0*pi/OmegaR;
+fR = 1.0/TR;
 % OmegaM = 500*2.0*pi; 
 % fM = OmegaM/(2*pi);
-% 
-% Tint=27*TR;
-% ODT=Tint/NFFT;
-% OTime = ODT*(0:NFFT-1);
-% 
-% 
-% 
-% figure(1)
-% hold on
-% grid on
-% f=load('IFFT.txt'); %IFFT data from C code
-% f1=f(:,1)*NFFT;
+
+Tint=TR;
+ODT=Tint/NFFT;
+OTime = ODT*(0:NFFT-1);
+
+
+
+figure(1)
+hold on
+grid on
+f=load('IFFT.txt'); %IFFT data from C code
+f1=f(:,1)*NFFT;
 % 
 % 
 % thref=importdata('Subrotatingmonopoletimehistory000.dat');
 % plot(thref(:,1),thref(:,2),'ko');
-% plot(OTime*1000+8.17,2*f1','r-');
+plot(OTime,f1','r-');
 % legend('Poletti et al','Predicted')
 % 
 % xlabel('{\itt} [ms]')
@@ -47,18 +47,19 @@ set(0,'DefaultaxesFontSize',15)
 % %set(gca,'XTick',(165:5:205))
 % %set(gcf, 'PaperPositionMode','Auto')   % Use screen size
 % 
-% Filename1 = ['timehistory_',num2str(10*MaX),num2str(10*MaY),num2str(10*MaZ)];
-% print(Filename1,'-depsc'); 
+Filename1 = ['timehistory_',num2str(10*MaX),num2str(10*MaY),num2str(10*MaZ)];
+print(Filename1,'-depsc'); 
 % 
 % 
 % %
 % %Comparision of pressure spectrum.
 % %
 % refSp=importdata('Subrotatingmonopolespectra000N.dat');
+figure(2)
 Pspec=importdata('FDPressureSpectrum.txt');
 f=Pspec(:,1);
 pF=Pspec(:,2);
-plot(f,pF);
+stem(f,pF);
 % 
 % 
 % k = 0;
@@ -95,7 +96,7 @@ plot(f,pF);
 % 
 % axis([0 1500 -0.0001 0.02])
 % 
-% set(gcf, 'PaperPositionMode','Auto')   % Use screen size
-% Filename2 = ['spectrum_',num2str(10*MaX),num2str(10*MaY),num2str(10*MaZ)];
-% print(Filename2,'-depsc'); 
+set(gcf, 'PaperPositionMode','Auto')   % Use screen size
+Filename2 = ['spectrum_',num2str(10*MaX),num2str(10*MaY),num2str(10*MaZ)];
+print(Filename2,'-depsc'); 
 
